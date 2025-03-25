c94df576f272a23b32e45a094aa8a73b6cb75033
ishini shavindhya <ishinishavindhya@gmail.com>
2025-03-25 22:48:17 +0530

    Header.css

--- frontend/src/componenets/Header/Header.jsx
+++ frontend/src/componenets/Header/Header.jsx
@@ -4,7 +4,11 @@ import './Header.css'
 function Header() {
   return (
     <div className='header'>
-      
+      <div className='header-contents'>
+      <h2>Order your favourite food here</h2>
+      <p>Food odors have been shown to influence food choices, portion selection, and can promote a specific desire to consume certain foods.</p>
+      <button>View Menu</button>
+      </div>
     </div>
   )
 }

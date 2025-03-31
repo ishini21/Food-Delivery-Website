a78002503ab728dcbbb38f820c9bae6c3b3ecbc8
ishini shavindhya <ishinishavindhya@gmail.com>
2025-03-31 12:31:35 +0530

    menulist

--- frontend/src/componenets/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/componenets/ExploreMenu/ExploreMenu.jsx
@@ -2,12 +2,23 @@ import React from 'react'
 import './ExploreMenu.css'
 import { menu_list } from '../../assets/assets'
 
-function ExploreMenu() {
+function ExploreMenu({category,setCategory}) {
   return (
     <div className='explore-menu' id='explore-menu'>
       <h1>Explore our menu</h1>
-     
-      
+     <p className='explore-menu-text'>Our partner restaurants maintain a 4.8+ hygiene rating—because safety tastes better.Your food arrives hot and fresh—guaranteed or your next meal is on us.</p>
+      <div className="explore-menu-list">
+        {menu_list.map((item,index)=>{
+          return(
+            <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)}key={index} className='explore-menu-list-item'>
+              <img className={category===item.menu_name?"active":""}src={item.menu_image} alt=""/>
+              <p>{item.menu_name}</p>
+
+            </div>
+          )
+        })}
+      </div>
+      <hr/>
     </div>
   )
 }
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css'
 import Header from '../../componenets/Header/Header'
+import ExploreMenu from '../../componenets/ExploreMenu/ExploreMenu'
+
 
 
 function Home() {
+
+  const [category,setCategory]= useState("All");
+
   return (
     <div>
       <Header/>
+      <ExploreMenu category={category} setCategory={setCategory}
+      />
+
     </div>
   )
 }

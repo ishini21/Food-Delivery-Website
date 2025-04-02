61aa2e4a3447f8385d16e02b40f2e48525e45dc5
ishini shavindhya <ishinishavindhya@gmail.com>
2025-04-02 09:18:21 +0530

    commit

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -4,11 +4,12 @@ import fs from 'fs'
 //Add food
 export const  addFood = async (req,res) => {
 
+    
     let image_filename = `${req.file.filename}`;
 
     const food = new foodModel({
         name:req.body.name,
-        description:req.bod.description,
+        description:req.body.description,
         price:req.body.price,
         category:req.body.category,
         image:image_filename,

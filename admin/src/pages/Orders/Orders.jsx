a31f3cc0dd9bd3ae721444a25e2f9f6bdc535e41
ishini shavindhya <ishinishavindhya@gmail.com>
2025-04-20 23:51:45 +0530

    display orders

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './Order.css'
 import { useState } from 'react'
 import axios from "axios";
+import { toast } from 'react-toastify';
+import { useEffect } from 'react';
 
 function Orders({url}) {
 
@@ -10,12 +12,21 @@ function Orders({url}) {
   const fetchAllOrders = async () => {
 
     const response = await axios.get(url+"/api/order/list");
-
+    if(response.data.success){
+      setOrders(response.data.data);
+      console.log(response.data.data)
+    }else{
+      toast.error("Error")
+    }
 
   }
+  useEffect(()=>{
+    fetchAllOrders();
+
+  },[])
   return (
     <div>
-      Orders
+     
     </div>
   )
 }
